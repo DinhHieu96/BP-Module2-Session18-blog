71036cc9731254cf403d4b8583f0021dab5fb8b9
DinhHieu96 <truongdinhhieu1496@gamil.com>
2019-10-17 10:27:56 +0700

    cm

---
+++ src/main/java/com/codegym/service/GeneralService.java
@@ -0,0 +1,10 @@
+package com.codegym.service;
+
+import java.util.List;
+
+public interface GeneralService<E> {
+    List<E> findAll();
+    void save(E e);
+    void delete(int index);
+    E findById(int id);
+}
